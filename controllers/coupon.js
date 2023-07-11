0503624997a8bc786459758388f59219901c2698
anthony <anthony0813@gmail.com>
2023-07-10 23:13:21 -0500

    ultimates features

--- controllers/coupon.js
+++ controllers/coupon.js
@@ -3,10 +3,18 @@ import { v4 as uuidv4 } from "uuid";
 import { mesesArr } from "../helpers/meses.js";
 import Coupon from "../models/Cuopon.js";
 
-export const getCoupons = (req, res) => {
-  res.json({
-    message: "all coupons",
-  });
+export const getCoupons = async (req, res) => {
+  try {
+    const cupons = await Coupon.find();
+    return res.json({
+      message: "all coupons",
+      cupons,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: "Error " + error.message,
+    });
+  }
 };
 
 export const createCoupons = async (req = request, res = response) => {
--- models/Cuopon.js
+++ models/Cuopon.js
@@ -1,20 +1,30 @@
 import { Schema, model } from "mongoose";
 
-const CouponSchema = Schema({
-  code: {
-    type: String,
-    required: false,
+const CouponSchema = Schema(
+  {
+    code: {
+      type: String,
+      required: false,
+    },
+    valid: {
+      type: Boolean,
+      required: false,
+      default: true,
+    },
   },
-  valid: {
-    type: Boolean,
-    required: false,
-    default: true
+  {
+    timestamps: true,
   }
-}, {
-  timestamps: true
-});
+);
 
-
+CouponSchema.methods.toJSON = function () {
+  const { _id: id, code, valid, ...rest } = this.toObject();
+  return {
+    id,
+    code,
+    valid,
+  };
+};
 const Coupon = model("Coupon", CouponSchema);
 
 export default Coupon;
